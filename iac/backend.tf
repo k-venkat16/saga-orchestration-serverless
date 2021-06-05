43a169c3b1bbd16efd23a1f034a1dcbe5a1c45c5
ManojG1978 <manojgr@gmail.com>
2021-06-05 08:22:23 +0530

    To trigger action

--- iac/backend.tf
+++ iac/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     # modify the values for the storage account
     storage_account_name = "sagatsstatemg"
-    container_name       = "terraformstate"
+    container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
 }
